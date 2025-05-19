d5647cba813bc0670c52e4c173942d76e9ddacad
gh286991 <gh286991@gmail.com>
2025-05-19 16:55:21 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-});
-module.exports = withMDX({
+module.exports = {
   output: 'export',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-}); 
+};
